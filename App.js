4dba712617f45be42213fd15adb8f13a34ac6280
AlexChepikov <a.m.chepikov@yandex.ru>
2018-12-18 17:12:43 +0300

    First version

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>КИСУНДРИЛЬ Я ТЕБЯ ЛЮБЛЮ!ВЫХОДИ ЗА МЕНЯ ЗАМУЖ!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    textAlign:'center'
+  }
+});
