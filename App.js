7ed6dcfd52fd553df93069307c7a11c2a7871ec5
AlexChepikov <a.m.chepikov@yandex.ru>
2018-12-25 17:15:06 +0300

    Add routing

--- App.js
+++ App.js
@@ -1,24 +1,130 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import Router from 'react-native-easy-router'
+import {Text, View, ScrollView} from 'react-native'
+import {Card, ListItem, Header, Button} from 'react-native-elements'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello world!</Text>
-      </View>
-    )
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+const list = [
+  {
+    title: 'Подписка',
+    icon: 'room'
   },
-  text: {
-    textAlign: 'center'
+  {
+    title: 'Прошлые номера',
+    icon: 'description'
+  },
+  {
+    title: 'Детские номера',
+    icon: 'description'
   }
-})
+]
+
+const Menu = ({router}) => (
+  <View style={{ flex: 1 }}>
+    <Header
+      backgroundColor='red'
+      centerComponent={{ text: 'Меню', style: { color: '#fff' } }}
+    />
+    <View>
+      {
+        list.map((item) => (
+          <ListItem
+            key={item.title}
+            title={item.title}
+            leftIcon={{name: item.icon, style: {color: 'red'}}}
+          />
+        ))
+      }
+    </View>
+    <Button
+      onPress={() => router.pop({type: 'right'})}
+      icon={{name: 'accessibility'}}
+      title='Назад'
+      buttonStyle={{marginTop: 10, marginBottom: 10}}
+    />
+  </View>
+)
+
+const First = ({ router }) => (
+  <View style={{ flex: 1 }}>
+    <Header
+      backgroundColor='red'
+      leftComponent={{ icon: 'menu', color: '#fff' }}
+      centerComponent={{ text: 'Главная страница', style: { color: '#fff' } }}
+      rightComponent={{ icon: 'shopping-cart', color: '#fff' }}
+    />
+    <Button
+      onPress={() => router.push.Menu({type: 'left'})}
+      icon={{name: 'accessibility'}}
+      title='Меню'
+    />
+    <Button
+      onPress={() => router.push.Second({type: 'left'})}
+      icon={{name: 'accessibility'}}
+      title='Каталог'
+    />
+  </View>
+)
+
+const Second = ({ router}) => (
+  <View style={{ flex: 1 }}>
+    <Header
+      backgroundColor='red'
+      leftComponent={{ icon: 'menu', color: '#fff' }}
+      centerComponent={{ text: 'Каталог', style: { color: '#fff' } }}
+      rightComponent={{ icon: 'shopping-cart', color: '#fff' }}
+    />
+    <ScrollView>
+      <Card
+        title='1000 Советов'
+        image={{uri: 'http://yuniline.ru/img/products/pr_1000-sovetov.png'}}
+        imageStyle={{width: '100%', height: 'auto'}}
+        style={{ flex: 1,
+          width: null,
+          height: null,
+          resizeMode: 'cover'}}
+      >
+        <Text style={{marginBottom: 10}}>
+          2 раза в месяц - 100 руб.
+        </Text>
+        <Button
+          icon={{name: 'keyboard-arrow-right'}}
+          backgroundColor='red'
+          buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+          title='Побробнее'
+        />
+      </Card>
+
+      <Card
+        title='1000 Советов'
+        image={{uri: 'http://yuniline.ru/img/products/pr_1000-sovetov.png'}}
+        imageStyle={{width: '100%', height: 'auto'}}
+        style={{ flex: 1,
+          width: null,
+          height: null,
+          resizeMode: 'cover'}}
+      >
+        <Text style={{marginBottom: 10}}>
+          2 раза в месяц - 100 руб.
+        </Text>
+        <Button
+          icon={{name: 'keyboard-arrow-right'}}
+          backgroundColor='red'
+          buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+          title='Побробнее'
+        />
+      </Card>
+      <Button
+        onPress={() => router.push.Menu({type: 'left'})}
+        icon={{name: 'accessibility'}}
+        title='Меню'
+        buttonStyle={{marginTop: 10, marginBottom: 10}}
+      />
+    </ScrollView>
+
+  </View>
+)
+
+const routes = { First, Second, Menu }
+const App = () => <Router routes={routes} initialRoute="First" />
+
+export default App
